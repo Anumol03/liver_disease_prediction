d09e364bf196af500a160bbcf92e5dac722dcaed
Anumol03 <anumol030101@gmail.com>
2024-08-19 08:09:37 +0530

    diabets

--- liver_disease/settings.py
+++ liver_disease/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-37iz(z*3s_jpu0ur$4@bl$k-!^gep(pw-#7gbei$^!$u6qo=y^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+    
+]
 
 
 # Application definition
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_ALLOW_ALL=True
+
+
